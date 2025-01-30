e04396dcd46db2f7d64b62b4d5fc53c7365c6556
theo-mandelbaum <mandelbaumtheo@gmail.com>
2025-01-30 13:48:09 -0500

    added base files-Part 2 finished

---
+++ catalog/urls.py
@@ -0,0 +1,8 @@
+# Create a urls file in catalog directory. This file will contain the URL mapping for the catalog application
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+]
